b7ecc541060465ca221c45a2fe35961287aa1923
Siguza <siguza@siguza.net>
2017-07-17 00:30:12 +0200

    add mach errors

--- cmds/strerror.c
+++ cmds/strerror.c
@@ -1,14 +1,38 @@
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <mach/mach.h>
 
 int main(int argc, const char **argv)
 {
-    if(argc < 2)
+    int off = 1;
+#ifndef NO_MACH
+    bool mach = false;
+#endif
+    for(; off < argc; ++off)
+    {
+        if(argv[off][0] != '-' || (argv[off][0] >= '0' && argv[off][0] <= '9')) break;
+#ifndef NO_MACH
+        if(strcmp(argv[off], "-m") == 0) mach = true;
+#endif
+        else
+        {
+            fprintf(stderr, "[!] Invalid argument: %s\n", argv[off]);
+            return 1;
+        }
+    }
+    if(argc - off < 1)
     {
         fputs("[!] Too few arguments.\n", stderr);
         return 1;
     }
-    printf("%s\n", strerror(atoi(argv[1])));
+    int i = atoi(argv[off]);
+    const char *s =
+#ifndef NO_MACH
+    mach ? mach_error_string(i) :
+#endif
+    strerror(i);
+    printf("%s\n", s);
     return 0;
 }
